6cd90d89de2a61c4afa1e682b3d5e03efb4132be
elhova.ad <elhova.ad@lsrgroup.ru>
2022-01-31 15:53:43 +0300

    Решение четвертой задачи

--- ex4/__init__.py
+++ ex4/__init__.py
@@ -1,3 +1,6 @@
+from data import EMPLOYEES
+from data import DEPARTMENTS
+
 def cross_join(employees, departments):
     """
     Реализует декартово произведение списков employees и departments
@@ -6,5 +9,15 @@ def cross_join(employees, departments):
     :param departments: Список DepartmentName таблицы Department
     :return: Генератор пар (LastName, DepartmentName)
     """
-    # put your code here
-    pass
+    indexEmployer = 0
+    indexDepartment = 0
+    while indexEmployer < len(employees):
+        while indexDepartment < len(departments):
+            yield (employees[indexEmployer], departments[indexDepartment])
+            indexDepartment += 1
+        indexEmployer += 1
+        indexDepartment = 0
+
+cr = cross_join(EMPLOYEES, DEPARTMENTS)
+print(next(cr))
+print(next(cr))
