6eb99493bcb6847b9decede6f0ade583dc1ce6e3
elhova.ad <elhova.ad@lsrgroup.ru>
2022-02-02 15:11:29 +0300

    Решение третий задачи

--- ex3/action.py
+++ ex3/action.py
@@ -5,6 +5,31 @@ class BaseAction:
     def __repr__(self):
         return self.name
 
+    def __gt__(self, other):
+        if self.name == 'Rock':
+            if other.name == 'Scissors':
+                return True
+        if self.name == 'Paper':
+            if other.name == 'Rock':
+                return True
+        if self.name == 'Scissors':
+            if other.name == 'Paper':
+                return True
+
+    def __lt__(self, other):
+        if self.name == 'Rock':
+            if other.name == 'Paper':
+                return True
+        if self.name == 'Paper':
+            if other.name == 'Scissors':
+                return True
+        if self.name == 'Scissors':
+            if other.name == 'Rock':
+                return True
+
+    def __eq__(self, other):
+        return self.name == other.name
+
 
 class NothingAction(BaseAction):
     def __init__(self):
